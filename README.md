ad7b184565ea75addc7882ea4c6f9482feef315d
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-02-20 22:30:17 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MusicApp
+
+Written by Teo Dell'Amico for App Academy Week 4 Day 5 project. Goin fancy on this one
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicApp::Application.config.secret_key_base = '038c1d3383430db84aa2e1037c4a4d6b636a0f18b16b0c9e8f929d69d89d6cc45f1a9159e67db530068c886a5d1c07df5b2f8ab4ef3f133245df432de9e5d25e'
