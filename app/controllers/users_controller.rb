ccebbf23f951cfc5b566bee253c632a57c203afe
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-02-21 17:31:54 -0800

    Added activation field to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:email, :password, :admin)
+		params.require(:user).permit(:email, :password, :admin, :activated)
 	end
 
 end
