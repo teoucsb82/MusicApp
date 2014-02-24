c0241f6f2bab7bfc446516b5efc8377b5b6dbb3b
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-02-23 20:54:45 -0800

    Pushing for tumblr

---
+++ db/migrate/20140222020059_add_activation_token_to_user_mailer.rb
@@ -0,0 +1,4 @@
+class AddActivationTokenToUserMailer < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140222012950) do
+ActiveRecord::Schema.define(version: 20140222020059) do
 
   create_table "albums", force: true do |t|
     t.string   "title",                       null: false
